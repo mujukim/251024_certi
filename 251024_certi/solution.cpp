9db15085f9f0caef2f2477c19f07b3d4d514542a
Muju <anwn0414@naver.com>
2025-10-26 23:35:53 +0900

    문제 추가, calculate 함수 구현

--- 251024_certi/solution.cpp
+++ 251024_certi/solution.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <unordered_map>
 #define MAX_N 5000
+#define INF 10e8
 using namespace std;
 struct distST
 {
@@ -11,6 +12,8 @@ struct distST
 vector < pair<int, int>> _dist[MAX_N]; // ecity, dist
 unordered_map<int, distST> _manager;
 int _N, _capital;
+void function(int pos, int tdist, int maxdist, int end);
+void add(int mId, int sCity, int eCity, int mDistance);
 
 void init(int N, int mCapital, int k, int mID[], int sCity[], int eCity[], int mDistance[])
 {
@@ -54,7 +57,38 @@ void remove(int mId)
 	}
 }
 
+int _totaldist, _maxdist;
+
 int calculate(int mCity)
 {
-	return 0;
+	_totaldist = INF;
+	_maxdist = -1;
+	function(_capital, 0, -1, mCity);
+	return _maxdist;
+}
+
+void function(int pos, int tdist, int maxdist, int end)
+{
+	if (pos == end)
+	{
+		if (_totaldist > tdist)
+		{
+			_totaldist = tdist;
+			_maxdist = maxdist;
+		}
+		if ((_totaldist == tdist) && (_maxdist > maxdist))
+		{
+			_maxdist = maxdist;
+		}
+	}
+	for (int i = 0; i < _dist[pos].size(); i++)
+	{
+		int md = maxdist;
+		tdist += _dist[pos].at(i).second;
+		if (_dist[pos].at(i).second > maxdist)
+		{
+			md = _dist[pos].at(i).second;
+		}
+		function(_dist[pos].at(i).first, tdist, md, end);
+	}
 }
